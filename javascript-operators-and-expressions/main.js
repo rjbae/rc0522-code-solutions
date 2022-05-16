e1ae908de08cf9dd5d3bcb7416e2ca1e41a5cd9f
Ryan Bae <baeryan21@gmail.com>
2022-05-16 18:26:31 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,43 @@
+var width = 10;
+var height = 15;
+
+var area = width * height;
+console.log('value of area:', area);
+console.log('type of area:', typeof area);
+
+var bill = 200;
+var payment = 250;
+
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('type of change:', typeof change);
+
+var quizzes = 90;
+var midterm = 95;
+var final = 100;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Ryan ';
+var lastName = 'Bae';
+
+var fullName = firstName + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 8;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 200;
+var isSparta = (headCount === 300);
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
