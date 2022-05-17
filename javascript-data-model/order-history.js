faeced038ed2f326171736ffd9a1efd947db125c
Ryan Bae <baeryan21@gmail.com>
2022-05-17 17:48:00 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,77 @@
+var orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    totalCost: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'August 8, 2020',
+    productReview: false,
+    items: [
+      {
+        itemType: 'Book',
+        name: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Closed on Sep 7, 2020',
+        itemPrice: 31.55
+      }
+    ]
+  },
+  {
+    orderDate: 'July 19, 2020',
+    totalCost: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveryDate: 'July 20, 2020',
+    productReview: false,
+    items: [
+      {
+        itemType: 'Book',
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Closed on Aug 19, 2020',
+        itemPrice: 41.33
+      }
+    ]
+  },
+  {
+    orderDate: 'July 4, 2020',
+    totalCost: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: 'July 7, 2020',
+    productReview: false,
+    items: [
+      {
+        itemType: 'Gaming Accessory',
+        name: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        author: null,
+        returnWindow: 'Closed on Aug 5, 2020',
+        itemPrice: 15.98
+      }
+    ]
+  },
+  {
+    orderDate: 'July 3, 2020',
+    totalCost: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: 'July 5, 2020',
+    productReview: false,
+    items: [
+      {
+        itemType: 'Gaming Accessory',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        returnWindow: 'Closed on Aug 4, 2020',
+        itemPrice: 94.95
+      },
+      {
+        itemType: 'Book',
+        name: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindow: 'Closed on Aug 4, 2020',
+        itemPrice: 33.99
+      }
+    ]
+  }
+];
