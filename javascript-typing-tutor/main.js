471eed4cfc7a580b0485ae4204629164d4406b61
Ryan Bae <baeryan21@gmail.com>
2022-05-25 21:04:53 +0000

    Completed javascript-typing-tutor challenge.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,2 +1,15 @@
-var phrase = document.querySelector('span');
-phrase.innerText = 'grumpy wizards make toxic brew';
+var $letters = document.querySelectorAll('span');
+var phrase = 'grumpy wizards make toxic brew';
+var index = 0;
+
+document.addEventListener('keydown', keyEvent);
+
+function keyEvent(event) {
+  if (event.key === phrase[index]) {
+    $letters[index].className = 'correct';
+    index++;
+    $letters[index].className = 'underline-correct';
+  } else {
+    $letters[index].className = 'incorrect';
+  }
+}
