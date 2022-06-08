d39d82c9a5ceab04e527ed72d2c28711f88e2ca2
Ryan Bae <baeryan21@gmail.com>
2022-06-08 22:24:28 +0000

    Finished HTML and CSS.

---
+++ javascript-carousel/main.js
@@ -0,0 +1,4 @@
+// var $rightArrow = document.querySelector('.fa-chevron-right');
+// var $leftArrow = document.querySelector('.fa-chevron-left');
+// var $imageNodes = document.querySelectorAll('li');
+// var $dotNodes = document.querySelectorAll('span');
