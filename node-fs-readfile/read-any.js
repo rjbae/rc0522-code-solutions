8dbe8ed3c2131d98fa91ea3f3933396fd731e79c
Ryan Bae <baeryan21@gmail.com>
2022-06-29 23:24:46 +0000

    Completed node-fs-readfile exercise.

---
+++ node-fs-readfile/read-any.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
