143f469f34a9b9e09dfb756bae604532dde56cd8
Ryan Bae <baeryan21@gmail.com>
2022-07-13 23:27:54 +0000

    Completed react-props-and-expressions exercise.

---
+++ react-props-and-expressions/src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function CustomButton(props) {
+  return (
+    <button>{props.text}</button>
+  );
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+root.render(element);
