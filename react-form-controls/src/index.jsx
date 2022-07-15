73c90c037b6d0e00f840aa4d738e59c86e62b743
Ryan Bae <baeryan21@gmail.com>
2022-07-15 22:29:37 +0000

    Completed react-form-controls exercise.

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value, password: this.state.password });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ username: this.state.username, password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+        <input type='text' value={this.state.username} onChange={this.handleUsernameChange} ></input>
+        </label>
+        <label>
+          Password:
+        <input type='password' value={this.state.password} onChange={this.handlePasswordChange} ></input>
+        </label>
+        <button type='submit' value='Submit'>Sign Up</button>
+      </form>
+    );
+  }
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<RegistrationForm />);
