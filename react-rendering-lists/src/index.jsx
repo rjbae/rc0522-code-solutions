0073abc453aa3cb576ad5104850860a323ef7136
Ryan Bae <baeryan21@gmail.com>
2022-07-16 00:15:39 +0000

    Completed react-rendering-lists exercise.

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+const pokeList = pokedex.map(pokemon => {
+  return <li key={pokemon.number}>{pokemon.name}</li>;
+});
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(pokeList);
