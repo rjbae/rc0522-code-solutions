100fd110ca14e3fa8e1fb0ded9b5680cbc163166
Ryan Bae <baeryan21@gmail.com>
2022-07-19 16:45:36 +0000

    In progress.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 const accordionData = [
   {
-    name: 'HTML',
+    id: '1',
+    name: 'Hypertext Markup Language',
     description: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.'
   },
   {
-    name: 'CSS',
+    id: '2',
+    name: 'Cascading Style Sheet',
     description: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.'
   },
   {
-    name: 'JS',
+    id: '3',
+    name: 'JavaScript',
     description: 'JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries.'
   }
 ];
@@ -24,28 +27,28 @@ class Accordion extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  // handleClick(event) {
-  // }
+  handleClick(event) {
+    const accordionId = accordionData.id;
+    if (event.target.key === accordionId) {
+      this.setState({ isClicked: true });
+    } else {
+      this.setState({ isClicked: false });
+    }
+  }
 
   render() {
     const accordionInfo = accordionData.map(language => {
-      return <p key={language.name} className='accordion-text'>{language.description}</p>;
+      return (
+        <div key={language.id}>
+          <button>{language.name}</button>
+          <p className='accordion-text'>{language.description}</p>
+        </div>
+      );
     });
 
     return (
     <div className='container'>
-      <div>
-        <button className='HTML'>Hypertext Markup Language</button>
-        {accordionInfo}
-      </div>
-      <div>
-        <button className='CSS'>Cascading Style Sheet</button>
-        {accordionInfo}
-      </div>
-      <div>
-        <button className='JS'>JavaScript</button>
-        {accordionInfo}
-      </div>
+      {accordionInfo}
     </div>
     );
   }
