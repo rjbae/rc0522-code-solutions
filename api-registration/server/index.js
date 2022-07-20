b96d2ad476eefef3783cb19e35b49f556b447185
Ryan Bae <baeryan21@gmail.com>
2022-07-20 19:00:25 +0000

    Changed sql return statement.

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -30,7 +30,7 @@ app.post('/api/auth/sign-up', (req, res, next) => {
       const sql = `
         insert into "users" ("username", "hashedPassword")
         values ($1, $2)
-        returning *
+        returning "userId", "username", "createdAt"
       `;
       const params = [username, hashedPassword];
       db.query(sql, params)
