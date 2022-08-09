9fe777abb49d4ccfeb08b1e33540545405f29931
Ryan Bae <baeryan21@gmail.com>
2022-08-09 18:27:16 +0000

    Completed data-structures-stacks exercise.

---
+++ data-structures-stacks/insert-under-top.js
@@ -0,0 +1,8 @@
+/* exported insertUnderTop */
+
+function insertUnderTop(stack, value) {
+  if (stack.peek() === undefined) return;
+  const topValue = stack.pop();
+  stack.push(value);
+  stack.push(topValue);
+}
